d7c97cb2aa3c52935626619f22bcddb63ba4b417
Charlie_Ubuntu <charliecooper98@gmail.com>
2015-01-05 20:12:43 +0100

    Ant implementation

--- ApacheAnt/src/main/Main.java
+++ ApacheAnt/src/main/Main.java
@@ -1,7 +1,21 @@
 package main;
 
+import math.MyMath;
+
 public class Main {
+	/*
+	 * BUILDTOOL: used to automate repetitive tasks
+	 * 	- Often run headless from the command line
+	 * 	- Apache Ant, Maven and Gradle
+	 * 
+	 * Apache Ant: Another Neat Tool
+	 * 	- build.xml file
+	 *  	- tasks: small atomic tasks that should be performed
+	 *  	- targets: directly invoked by Ant, have dependencies
+	 *  	- extension points
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		MyMath math = new MyMath();
+		System.out.println("Result is: " + math.multi(5, 10));
 	}
 }
